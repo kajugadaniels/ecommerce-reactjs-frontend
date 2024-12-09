d072c1c8091f7185f7f199cd59a405cada5b4c4b
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-09 14:11:07 +0200

    Updated UserLayout to include ToastContainer and wrapped children with UserProvider.

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,16 +1,23 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import { UserProvider } from '@/contexts/userContext';
 import Image from 'next/image';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <UserProvider>
+      <div>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar newestOnTop closeOnClick pauseOnFocusLoss draggable pauseOnHover />
+      </div>
+    </UserProvider>
   );
 }
